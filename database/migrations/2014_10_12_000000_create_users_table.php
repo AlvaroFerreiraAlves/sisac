6fb5b1b72c8cd57b2df6cf45081fedaa52c7ea7a
RômuloSousa <romulo.rmos@hotmail.com>
2017-07-10 22:54:29 -0300

    Criação Migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('nome');
+            $table->float('matricula');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('status');
             $table->rememberToken();
             $table->timestamps();
         });
