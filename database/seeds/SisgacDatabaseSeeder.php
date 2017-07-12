aea6b433f853f4236ed8f5d956fb92e30bd85470
root <alvarof.alves@hotmail.com>
2017-07-11 21:41:28 -0300

    seed

---
+++ database/seeds/SisgacDatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SisgacDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(App\Entities\Course::class,5)->create();
+        factory(App\Entities\User::class,5)->create();
+        factory(App\Entities\Activity::class,10)->create();
+        factory(App\Entities\ActivityType::class,4)->create();
+        factory(App\Entities\UserTypesUser::class,5)->create();
+        factory(App\Entities\UserType::class,4)->create();
+        factory(App\Entities\Regulation::class,3)->create();
+    }
+}
