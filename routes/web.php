94688a2b4086171d5b2044e8d9dbc490a2d25bcf
AlvaroFerreiraAlves <alvarof.alves@hotmail.com>
2017-07-30 12:35:45 -0300

    Rotas do CRUD

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,14 @@ Route::post('tipo-atividade/create','ActivityTypesController@store');
 Route::put('tipo-atividade/{id}/update','ActivityTypesController@update');
 Route::delete('tipo-atividade/{id}/destroy','ActivityTypesController@destroy');
 
+Route::get('regulamentos/list','RegulationsController@index');
+Route::post('regulamentos/create','RegulationsController@store');
+Route::put('regulamentos/{id}/update','RegulationsController@update');
+Route::delete('regulamentos/{id}/destroy','RegulationsController@destroy');
+
+Route::get('cursos/list','CoursesController@index');
+Route::post('cursos/create','CoursesController@store');
+Route::put('cursos/{id}/update','CoursesController@update');
+Route::delete('cursos/{id}/destroy','CoursesController@destroy');
+
+
